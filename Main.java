73cb87b9bf5286064fc085e2d6aaee3ffee9907b
zlevinskas <59978861+zlevinskas@users.noreply.github.com>
2020-02-13 14:34:53 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+// This is a new file 
+
+// This is CS department of TSU, we teach software engineering in COMP 3300
+class Main 
+
+{
+public static void main(String[] args) 
+
+{
+
+Scanner Obj = new Scanner (System.in);
+System.out.println("Enter an integer value");
+
+int x_val = 10; //Obj.nextInt();
+int value = 10000;
+int count = 1234;
+
+while (x_val >= 1)
+{ 
+
+ val = (double)count/x;
+System.out.println(val);
+
+ value = value + x_val; 
+ count ++ ;
+
+ if (count == 5)
+ { 
+  x_val = 4;
+ }
+ else 
+{ 
+  x_val -= 2;
+}
+
+System.out.println("value"+value);  
+System.out.println("Count"+count);
+System.out.println("x"+ x_val);  
+}  
+System.out.println("DONE!");  
+
+for (int q = x_val; q<=8; q++)
+{
+
+if (q == 4)
+       {
+         q = 5;
+       }
+
+       
+}
+
+
+}
+
+}
